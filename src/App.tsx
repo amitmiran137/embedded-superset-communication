e1911a3c02ae8be19639497f5ffbbfd96145fe44
amitmiran137 <amit.miran@nielsen.com>
2021-05-30 10:10:18 +0300

    feat: add Input above  iframe

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,29 @@ import styled from 'styled-components'
 import './App.css';
 
 
+const Wrapper = styled.div`
+  height: 100%;
+`
+export const FlexContainer = styled.div`
+  display: flex;
+`
 
+const InputWrapper = styled(FlexContainer)`
+  margin-bottom: 10px;
+  align-items: flex-end;
+
+  .infra-action-icon-wrapper {
+    margin-left: auto;
+  }
+`
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color:  "palevioletred";
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
 const EmbeddedIframe = styled.iframe`
   // 48px is the height of the dropdowns + the margin bottom it has
   height: calc(100% - 48px);
@@ -18,19 +40,23 @@ export const SUPERSET_FRAME_NAME = 'supersetFrame'
 export const supersetUrl =
     // @ts-ignore
     // eslint-disable-next-line camelcase
-    'https://localhost:8088'
+    'http://localhost:8088'
 
 const id_or_slug=4
 
 function App() {
     return (
-        <React.Fragment>
+        <Wrapper>
+            <InputWrapper>
+            <Input
+            />
+            </InputWrapper>
             <EmbeddedIframe
                 name={SUPERSET_FRAME_NAME}
                 title='superset'
                 src={`${supersetUrl}/superset/dashboard/${id_or_slug}/?standalone=2`}
             />
-        </React.Fragment>
+        </Wrapper>
     )
 }
 
